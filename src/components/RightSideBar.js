a1c9e01685df6baf2b643956e87249d991d52df1
Alvin Cousins <alvin.cousins@googlemail.com>
2021-06-28 18:46:49 +0100

    Signature tile

--- src/components/RightSideBar.js
+++ src/components/RightSideBar.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import WhoToFollow from "./WhoToFollow";
+import Signature from "./Signature";
 
 const RightSideBar = (props) => {
   return (
     <div className='rightsidebar'>
       <WhoToFollow fetchUser={props.fetchUser} />
+      <Signature />
     </div>
   );
 };
---
+++ src/components/Signature.js
@@ -0,0 +1,27 @@
+const Signature = () => {
+  return (
+    <div className='signature'>
+      <div>alvin.cousins@googlemail.com</div>
+      <div>
+        <strong className='signature__label'>Back-end:</strong>&nbsp;
+        <a
+          className='signature__link'
+          href='https://github.com/ajcousins/odinbook'
+        >
+          github.com/ajcousins/odinbook
+        </a>
+      </div>
+      <div>
+        <strong className='signature__label'>Front-end:</strong>&nbsp;
+        <a
+          className='signature__link'
+          href='https://github.com/ajcousins/odinbook-react'
+        >
+          github.com/ajcousins/odinbook-react
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Signature;
